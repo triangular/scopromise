9ec1d4225b15e61dff1a3687bed67370117d0241
Jakub Strojewski <jakub.strojewski@gmail.com>
2014-11-26 08:59:39 +0100

    Cleanup

--- dist/tri-angular-scopromise.js
+++ dist/tri-angular-scopromise.js
@@ -1,17 +1,18 @@
 (function (ng, app) {
     'use strict';
 
+
     /**
-     * shortcuts:
+     * Shortcuts
      */
     var _isArray = ng.isArray;
     var _forEach = ng.forEach;
     var _extend = ng.extend;
 
+
+
     /**
-     * @ngdoc   function
-     * @name    _shallowClear
-     * @param   {Object|Array} dst
+     * Clear the fulfillment placeholder
      */
     var _shallowClear = function (dst) {
 
@@ -34,11 +35,10 @@
         return dst;
     };
 
+
+
     /**
-     * @ngdoc   function
-     * @name    _shallowCopy
-     * @param   {Object|Array} src
-     * @param   {Object|Array} dst
+     * Create a shallow copy of an object or array - fill fulfillment placeholder with real data
      */
     var _shallowCopy = function (src, dst) {
         var key;
@@ -54,36 +54,75 @@
         return dst;
     };
 
+
+
     /**
-     * @ngdoc   service
-     * @name    $scopromise
+     * @ngdoc module
+     * @name triNgScopromise
+     * @description
+     *
+     * # triNgScopromise
+     *
+     * The `triNgScopromise` module provides support for any `promised` data
+     * that may be bound to scope. It's inspired by AngularJS `$resource` service.
+     *
      */
+
+    /**
+     * @ngdoc service
+     * @name $scopromise
+     * @requires $q, $log
+     *
+     * @description
+     *
+     *
+     * @param {Object|Array} [model] Model to be wrapped in 'Future'
+     * @param {Promise|Object} promise Future resolving PromiseLikeThingOrAnyThing
+     *
+     * @returns {Object|Array} fulfillment An object or array that is placeholder
+     *   for promised data.
+     */
+
     app.factory('$scopromise', ['$q', '$log', function ($q, $log) {
-        /**
-         * @param   {Object}            [model]      Model to be wrapped in 'Future'
-         * @param   {Promise|Object}    promise      Future resolving PromiseLikeThingOrAnyThing
-         */
-        return function (model, promise) {
-            if (!model && !promise) {
+        return function () {
+            var promise, model;
+
+            if (!arguments[0] && !arguments[1]) {
                 $log.error(new Error('$scopromise needs at least one argument!'));
             }
 
             // pass anything if using one arg
-            if (!promise) {
-                promise = model;
+            if (!arguments[1]) {
+
+                // any promise like thing or just data
+                promise = $q.when(arguments[0]);
+
+                // empty data placeholder for upcoming fulfillment
                 model = {};
+
+            } else {
+
+                // any promise like thing or just data
+                promise = $q.when(arguments[1]);
+
+                // user defined data placeholder for upcoming fulfillment
+                model = arguments[0];
+
             }
 
-            // any promise like thing or just data
-            promise = $q.when(promise);
-
             _extend(model, {
-                $promise: promise.then(
-                    function (data) {
-                        // just extending promise and resolved or override old model
-                        return data === model ? data : _shallowCopy(data, _shallowClear(model));
-                    }, $q.reject),
+
+                // anyone can use promise in future
+                $promise: promise.then(function (data) {
+
+                    // just extending promise and resolved or override old model
+                    return data === model ? data : _shallowCopy(data, _shallowClear(model));
+
+                }, $q.reject), // AngularJS $q does not automatically pass a rejection, so it's handled explicitly
+
+                // hold state of our fulfillment for outer world ($scope)
                 $resolved: false
+
             });
 
             model.$promise['finally'](function () {
